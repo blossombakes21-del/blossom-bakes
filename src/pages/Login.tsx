5d2ab7af5099afa4567425407b33763f4e1ec3a0
Sachin Raju <sachin.raju@INTERLINK.COM>
2026-08-18 02:45:25 +0530

    Fix React missing namespace error

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { Croissant, LogIn } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
